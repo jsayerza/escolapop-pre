0d37babbc82fb00b911d2dde04b2f3eae71d25f9
jsayerza <jsayerza.comptes@gmail.com>
2022-05-16 20:07:34 +0200

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -9,15 +8,3 @@ module.exports = {
   },
   plugins: [],
 };
-=======
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
->>>>>>> 1348e8c4bfc308586ebb73d5c227536bdac43d41
