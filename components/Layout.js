f66b290b0ea6a86e7fa8649f379751af75dac1f8
joaquinns <joaquindevv96@gmail.com>
2022-05-20 17:06:14 -0400

    added global height to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,17 @@
-import { ToastContainer } from "react-toastify"
-import { Navbar } from "./Navbar"
+import { ToastContainer } from "react-toastify";
+import { Navbar } from "./Navbar";
 
-export function Layout({children}) {
+export function Layout({ children }) {
   return (
     <>
-        <Navbar />
-        
-        <div className="bg-gray-100 h-screen p-10">
-            <div className="container mx-auto h-full">
-                {children}
-            </div>
-        </div>
-        <ToastContainer/>        
+      <Navbar />
 
+      <div className="bg-gray-100 h-full p-10">
+        <div className="container mx-auto h-full">{children}</div>
+      </div>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
