d01f048f665d244f54531668811129548a5bedc9
joaquinns <joaquindevv96@gmail.com>
2022-05-20 17:09:03 -0400

    created search page

---
+++ pages/articles/search/[search].js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { HOST_SV } from "../../../config/config";
+import { Layout } from "../../../components/Layout";
+import SearchBar from "../../../components/SearchBar";
+import { SearchResults } from "../../../components/SearchResults";
+
+function Search({ searchQuery, search, queryObj }) {
+  console.log(searchQuery, search);
+  return (
+    <Layout>
+      <SearchBar queryObj={queryObj} />
+      <h1>Search :D of {search}</h1>
+
+      <SearchResults searched={searchQuery} />
+    </Layout>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+  console.log(context.query?.search?.category, "in the server response");
+  const { search } = context.query;
+  const queryObj = context.query;
+  const { data: searchQuery } = await axios.get(
+    HOST_SV + "/api/articles/search/" + context.query.search,
+    {
+      params: {
+        category: context.query?.category,
+        price: context.query?.price,
+        location: context.query?.location,
+      },
+    }
+  );
+
+  return {
+    props: {
+      searchQuery,
+      search,
+      queryObj,
+    },
+  };
+};
+
+export default Search;
