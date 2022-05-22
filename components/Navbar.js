f89e4be98700110e0725b357e5e55e80a8dd80d2
joaquinns <joaquindevv96@gmail.com>
2022-05-22 15:06:00 -0400

    fixed styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -25,7 +25,7 @@ export function Navbar() {
   );
 
   return (
-    <nav className="bg-emerald-600 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 fixed top-0 left-0 right-0 h-16 z-30">
+    <nav className="bg-emerald-600 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 fixed top-0 left-0 right-0 h-16 z-50">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link href="/">
           <a className="flex items-center">
