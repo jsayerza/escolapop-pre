03e93eff4c9bf61ca873d10de5c97865a0afa8c4
joaquinns <joaquindevv96@gmail.com>
2022-05-22 15:06:23 -0400

    added styles logic and isOpen prop

--- components/ButtonFilter.js
+++ components/ButtonFilter.js
@@ -1,20 +1,32 @@
-export const ButtonFilter = ({ children, handlerClick, active }) => {
+import clsx from "clsx";
+
+export const ButtonFilter = ({ children, handlerClick, active, isOpen }) => {
+  const classesActive = clsx(
+    "relative px-4 py-2 bg-gray-200 font-semibold rounded duration-200 transition-colors shadow-md hover:bg-gray-100",
+    {
+      ["z-30"]: isOpen === true,
+      ["z-10"]: isOpen === false,
+    }
+  );
+
+  const classNames = clsx(
+    `relative px-4 py-2 bg-gray-100 font-semibold rounded duration-200 transition-colors shadow-md hover:bg-gray-200`,
+    {
+      ["z-30"]: isOpen === true,
+      ["z-10"]: isOpen === false,
+    }
+  );
+
   if (active) {
     return (
-      <button
-        className="relative px-4 py-2 bg-gray-200 font-semibold rounded z-20 duration-200 transition-colors shadow-md hover:bg-gray-100"
-        onClick={handlerClick}
-      >
+      <button className={classesActive} onClick={handlerClick}>
         {children}
       </button>
     );
   }
 
   return (
-    <button
-      className="relative px-4 py-2 bg-gray-100 font-semibold rounded z-20 duration-200 transition-colors shadow-md hover:bg-gray-200"
-      onClick={handlerClick}
-    >
+    <button className={classNames} onClick={handlerClick}>
       {children}
     </button>
   );
