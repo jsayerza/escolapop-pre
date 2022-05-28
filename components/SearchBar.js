095645cacf8f33fffba7203fb6fc0a0b026c5bef
joaquinns <joaquindevv96@gmail.com>
2022-05-28 19:41:40 -0400

    added filter button to search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,6 +1,8 @@
 import { useState, useRef } from "react";
 import { useRouter } from "next/router";
 import { SearchIcon } from "../icons/SearchIcon";
+import { IoIosCloseCircleOutline, IoIosCloseCircle } from "react-icons/io";
+import { IoOptionsOutline } from "react-icons/io5";
 import { HOST_SV } from "config/config";
 import { SearchFilterBar } from "./SearchFiltersBar";
 import { useShowComponent } from "hooks/useShowComponent";
@@ -9,7 +11,7 @@ export default function SearchBar({ keyword, queryObj, filters }) {
   console.log(queryObj, "form the searchBar component");
   const router = useRouter();
   const [search, setSearch] = useState("");
-  const inputRef = useRef("");
+  const inputRef = useRef(null);
   const [show, handleShowComponent] = useShowComponent();
 
   const handleSubmit = (e) => {
@@ -40,15 +42,23 @@ export default function SearchBar({ keyword, queryObj, filters }) {
         </form>
         {filters && (
           <button
-            className="px-2 py-2 font-bold text-white bg-orange-500 rounded hover:bg-orange-600 transition-all duration-200"
+            className="flex flex-row-reverse items-center gap-1 px-2 md:px-4 py-2 md:text-lg font-bold text-white bg-orange-500 rounded hover:bg-orange-600 transition-all duration-200"
             onClick={handleShowComponent}
           >
-            Filters
+            <h2 className="hidden md:block">Filtres</h2>
+            <span>
+              <IoOptionsOutline size={"1.8rem"} />
+            </span>
+            {/* show && (
+              <span className="bg-transparent rounded-full font-bold">
+                <IoIosCloseCircle size={"1.8rem"} />
+              </span>
+            ) */}
           </button>
         )}
       </div>
       {show && (
-        <div className="p-4">
+        <div className="py-2">
           <SearchFilterBar keyword={keyword} queryObj={queryObj} />
         </div>
       )}
