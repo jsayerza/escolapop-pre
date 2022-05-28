323707dc67d6ab515395cdd39d553b743409bed3
joaquinns <joaquindevv96@gmail.com>
2022-05-28 19:40:50 -0400

    added mobile style

--- components/Modal.js
+++ components/Modal.js
@@ -11,7 +11,7 @@ export const Modal = ({ children, isOpen, closeModal }) => {
       onClick={closeModal}
     >
       <div
-        className="z-30 absolute top-40 left-10 grid justify-center items-center gap-2 w-4/5 md:w-2/4 p-2 bg-white shadow-xl rounded h-96 overflow-y-auto"
+        className="z-30 absolute top-44 left-10 grid justify-center items-center gap-2 w-4/5 md:w-2/4 p-2 bg-white shadow-xl rounded h-96 overflow-y-auto"
         onClick={handleModalClick}
       >
         {children}
