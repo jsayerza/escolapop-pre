8cdbda705a1db6c6462695d779f81b7b7bb1d4a0
joaquinns <joaquindevv96@gmail.com>
2022-06-04 14:57:57 -0400

    added articles logic

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -31,74 +31,83 @@ export default function ArticleList({ articles }) {
 
   return (
     <div className="flex gap-4 flex-col w-full justify-center">
-      {articles.map((article) => (
-        <div
-          className="flex flex-col md:flex-row justify-center md:justify-between items-center border-gray-200 border-b pb-2 px-2 gap-6 transition-all duration-200 hover:bg-white"
-          key={article.articleid}
-        >
-          <div className="flex flex-col md:flex-row md:items-center">
-            <div className="rounded">
-              {article && article.imageurl && (
-                <Image
-                  src={article.imageurl}
-                  width={300}
-                  height={200}
-                  objectFit="cover"
-                  className="max-w-full h-auto rounded-lg transition-shadow ease-in-out shadow-none"
-                  alt="image of the article"
-                />
-              )}
+      {articles &&
+        articles.map((article) => (
+          <div
+            className="flex flex-col md:flex-row justify-center md:justify-between items-center border-gray-200 border-b pb-2 px-2 gap-6 transition-all duration-200 hover:bg-white"
+            key={article.articleid}
+          >
+            <div className="flex flex-col md:flex-row md:items-center">
+              <div className="rounded">
+                {article && article.imageurl && (
+                  <Image
+                    src={article.imageurl}
+                    width={300}
+                    height={200}
+                    objectFit="cover"
+                    className="max-w-full h-auto rounded-lg transition-shadow ease-in-out shadow-none"
+                    alt="image of the article"
+                  />
+                )}
+              </div>
+
+              <div className="flex flex-col justify-center items-center py-4 px-8">
+                <h1 className="textl-lg font-lato font-normal text-gray-900">
+                  {article.articletitle}
+                </h1>
+                <p className="text-sm font-lato font-normal text-gray-500">
+                  {article.description}
+                </p>
+              </div>
+
+              <div className="flex flex-col justify-center items-center py-4 px-8">
+                <h1 className="text-lg font-lato font-normal">
+                  {article.publicationstatus}
+                </h1>
+              </div>
+
+              <div className="flex flex-col justify-center items-center py-4 px-8">
+                <h1 className="text-lg font-lato font-normal">
+                  {article.salestatus}
+                </h1>
+              </div>
+
+              <div className="flex flex-col justify-center items-center py-4 px-8">
+                <h1 className="textl-lg font-lato font-normal text-gray-900">
+                  Preu
+                </h1>
+                <h2 className="text-xl font-lato font-bold">
+                  {article.price}€
+                </h2>
+              </div>
+
+              <div className="flex flex-col justify-center items-center py-4 px-8">
+                <h1 className="text-lg font-lato font-normal">Modificat</h1>
+                <h2 className="font-lato font-bold text-gray-500">
+                  to/do/fecha
+                </h2>
+              </div>
             </div>
 
-            <div className="flex flex-col justify-center items-center py-4 px-8">
-              <h1 className="textl-lg font-lato font-normal text-gray-900">
-                {article.articletitle}
-              </h1>
-              <p className="text-sm font-lato font-normal text-gray-500">
-                {article.description}
-              </p>
-            </div>
-
-            <div className="flex flex-col justify-center items-center py-4 px-8">
-              <h1 className="text-lg font-lato font-normal">
-                {article.publicationstatus}
-              </h1>
-            </div>
-
-            <div className="flex flex-col justify-center items-center py-4 px-8">
-              <h1 className="text-lg font-lato font-normal">{article.salestatus}</h1>
-            </div>
-
-            <div className="flex flex-col justify-center items-center py-4 px-8">
-              <h1 className="textl-lg font-lato font-normal text-gray-900">Preu</h1>
-              <h2 className="text-xl font-lato font-bold">{article.price}€</h2>
-            </div>
-
-            <div className="flex flex-col justify-center items-center py-4 px-8">
-              <h1 className="text-lg font-lato font-normal">Modificat</h1>
-              <h2 className="font-lato font-bold text-gray-500">to/do/fecha</h2>
+            <div className="flex gap-4 justify-center items-center">
+              <button
+                className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-gray-400 hover:text-white transition-all duration-200"
+                onClick={() => {
+                  //console.log("ArticleView/article.articleid: ", article.articleid)
+                  router.push("/articles/edit/" + article.articleid);
+                }}
+              >
+                <FiEdit3 size={22} />
+              </button>
+              <button
+                className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-red-500 hover:text-white transition-all duration-200"
+                onClick={() => handleDelete(article.articleid)}
+              >
+                <AiFillDelete size={22} />
+              </button>
             </div>
           </div>
-
-          <div className="flex gap-4 justify-center items-center">
-            <button
-              className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-gray-400 hover:text-white transition-all duration-200"
-              onClick={() => {
-                //console.log("ArticleView/article.articleid: ", article.articleid)
-                router.push("/articles/edit/" + article.articleid);
-              }}
-            >
-              <FiEdit3 size={22} />
-            </button>
-            <button
-              className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-red-500 hover:text-white transition-all duration-200"
-              onClick={() => handleDelete(article.articleid)}
-            >
-              <AiFillDelete size={22} />
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
