e80648203643a0bd1e9331cd2e432e9ac8d9881c
joaquinns <joaquindevv96@gmail.com>
2022-06-04 14:59:59 -0400

    refactor queries to insert in mysql

--- pages/api/register/index.js
+++ pages/api/register/index.js
@@ -1,29 +1,31 @@
 import { pool } from "../../../config/db";
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   switch (req.method) {
     case "POST":
-      return await getRegister(req, res);
+      return getRegister(req, res);
+
     default:
-      return;
+      res.status(500).json({ server: "This method is not allowed" });
   }
 }
 
 const getRegister = async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email, id, username } = req.body;
 
     const [searchEmail] = await pool.query(
-      `SELECT * FROM user WHERE useremail = ${email}`
+      `SELECT * FROM user WHERE useremail = '${email}'`
     );
 
-    if (searchEmail !== null ?? searchEmail[0] !== []) {
+    if (searchEmail.length > 0) {
       return res.status(400).json({ message: "email already exists" });
     }
 
     const [result] = await pool.query(
-      `INSERT INTO user (useremail) VALUE ('${email}')`
+      `INSERT INTO user (useremail, fbkey, username) VALUES ('${email}','${id}','${username}')`
     );
+    console.log(result[0]);
     return res.status(201).json(result[0]);
   } catch (error) {
     return res.status(500).json({ message: error.message });
