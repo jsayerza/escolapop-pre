a385fceef9ca6b58f22037d0f5bffdea025e97cd
joaquinns <joaquindevv96@gmail.com>
2022-06-15 16:34:29 -0400

    added links to searchCategoryBar

--- components/SearchCategoryBar.js
+++ components/SearchCategoryBar.js
@@ -1,40 +1,55 @@
 import Image from "next/image";
-import { Tooltip } from '@mui/material';
-import { IconButton } from '@mui/material';
-
+import Link from "next/link";
+import { Tooltip } from "@mui/material";
+import { IconButton } from "@mui/material";
 
 export const SearchCategoryBar = () => {
-    return (
-        <div>
-            <Image
-                width={100}
-                height={100}
-                src="/escolapop-cat-roba.png"
-                alt="Categoria Roba"
-                className="h-10 w-10 rounded-full"
-            />
-            <Image
-                width={100}
-                height={100}
-                src="/escolapop-cat-llibres.png"
-                alt="Categoria Roba"
-                className="h-10 w-10 rounded-full"
-            />
-            <Image
-                width={100}
-                height={100}
-                src="/escolapop-cat-matesc.png"
-                alt="Categoria Roba"
-                className="h-10 w-10 rounded-full"
-            />
-            <Image
-                width={100}
-                height={100}
-                src="/escolapop-cat-matext.png"
-                alt="Categoria Roba"
-                className="h-10 w-10 rounded-full"
-            />
-        </div>
-        
-    )
+  return (
+    <div>
+      <Link href="/articles/search?category=Roba">
+        <a>
+          <Image
+            width={100}
+            height={100}
+            src="/escolapop-cat-roba.png"
+            alt="Categoria Roba"
+            className="h-10 w-10 rounded-full"
+          />
+        </a>
+      </Link>
+      <Link href="/articles/search?category=Llibres">
+        <a>
+          <Image
+            width={100}
+            height={100}
+            src="/escolapop-cat-llibres.png"
+            alt="Categoria Llibres"
+            className="h-10 w-10 rounded-full"
+          />
+        </a>
+      </Link>
+      <Link href="/articles/search?category=Material+escolar">
+        <a>
+          <Image
+            width={100}
+            height={100}
+            src="/escolapop-cat-matesc.png"
+            alt="Categoria Roba"
+            className="h-10 w-10 rounded-full"
+          />
+        </a>
+      </Link>
+      <Link href="/articles/search?category=Material+extraescolar">
+        <a>
+          <Image
+            width={100}
+            height={100}
+            src="/escolapop-cat-matext.png"
+            alt="Categoria Roba"
+            className="h-10 w-10 rounded-full"
+          />
+        </a>
+      </Link>
+    </div>
+  );
 };
