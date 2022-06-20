821c8d0edebfb790e46e72a877d706acc2088528
joaquinns <joaquindevv96@gmail.com>
2022-06-20 15:23:40 -0700

    added order by size and course button

--- components/OrderButton.js
+++ components/OrderButton.js
@@ -3,6 +3,8 @@ import { useHandlersFilters } from "hooks/useHandlersFilters";
 import { useModal } from "hooks/useModal";
 import { Modal } from "./Modal";
 import { BsArrowUpRight, BsArrowDownLeft } from "react-icons/bs";
+import { GiClothes } from "react-icons/gi";
+import { FaPencilAlt } from "react-icons/fa";
 
 export const OrderButton = ({ queryObj, keyword }) => {
   const [open, handleModal, handleCloseOrderModal] = useModal();
@@ -37,6 +39,20 @@ export const OrderButton = ({ queryObj, keyword }) => {
           >
             <BsArrowDownLeft size="2rem" />
             Ordenar mas baratos
+          </button>
+          <button
+            className="flex gap-2 items-center bg-slate-100 px-4 py-4 rounded font-semibold"
+            onClick={() => handleOrder("max_size")}
+          >
+            <GiClothes size="2rem" />
+            Ordenar por talla mas grande
+          </button>
+          <button
+            className="flex gap-2 items-center bg-slate-100 px-4 py-4 rounded font-semibold"
+            onClick={() => handleOrder("min_course")}
+          >
+            <FaPencilAlt size="2rem" />
+            Ordenar por curso
           </button>
         </div>
       </Modal>
