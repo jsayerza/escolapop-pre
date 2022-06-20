bc52eb7908631befbe3c1d29f626b0b9fbe377d9
joaquinns <joaquindevv96@gmail.com>
2022-06-19 21:55:18 -0700

    added tailwind styles

--- components/BadgeSaleStatus.js
+++ components/BadgeSaleStatus.js
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 export const BadgeSaleStatus = ({ status }) => {
   const classNamesDnD = clsx(
-    "rounded-full font-lato font-bold bg-greenescola px-3 py-1 text-white",
+    "rounded-full font-lato font-bold bg-greenescola px-2 md:px-3 py-1 text-xs md:text-md md: text-white",
     {
       ["bg-greenescola"]: status === "En venda",
       ["bg-orangeAMPA"]: status === "Reservat",
