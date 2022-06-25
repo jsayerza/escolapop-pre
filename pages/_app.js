d0062cd408d8f6469e236a6b9ffb0b739ed8368c
joaquinns <joaquindevv96@gmail.com>
2022-06-25 13:23:31 -0700

    remove next auth from app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
-import { SessionProvider } from "next-auth/react";
 import { AuthContextProvider } from "../context/authContext";
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session} refetchInterval={5 * 60}>
-      <AuthContextProvider>
-        <Component {...pageProps} />
-      </AuthContextProvider>
-    </SessionProvider>
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
   );
 }
 
