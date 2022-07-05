3b80f6fddca0d8c9c63dab4a07438ce517767bf7
joaquinns <joaquindevv96@gmail.com>
2022-07-05 10:20:07 -0400

    added query params to button

--- components/FavoriteButtons.js
+++ components/FavoriteButtons.js
@@ -12,7 +12,6 @@ import { AiFillDelete } from "react-icons/ai";
 import { HOST_SV } from "../config/config";
 import { BadgeStatus } from "../components/BadgeStatus";
 
-
 export const FavoriteButtons = ({ publicationStatus, saleStatus, article }) => {
   const router = useRouter();
   const { user } = useUser();
@@ -21,28 +20,22 @@ export const FavoriteButtons = ({ publicationStatus, saleStatus, article }) => {
     try {
       //console.log("ArticleList/handleDeleteFavorite/articleid: ", articleid);
       //console.log("ArticleList/handleDeleteFavorite/user.email: ", user.email);
-
-      return await axios
-        .delete(HOST_SV + `/api/articles/favorites`, { articleid, useremail: user.email })
-        .then((res) => {
-            console.log("ArticleList/handleDeleteFavorite/success!");
-            //toast.success("Article eliminat de la llista de favorits");
-            //router.push("/");
-        })
-        .catch((e) => console.log("handleDeleteFavorite delete article error: ", e));
+      console.log(articleid);
+      return await axios.delete(
+        HOST_SV + `/api/articles/favorites?aid=${articleid}&email=${user.email}`
+      );
     } catch (error) {
       //console.log("ArticleList/handleDeleteFavorite/error: ", error);
       toast.error(error.response.data.message);
     }
   };
 
-
   return (
     <div className="flex flex-wrap md:flex-nowrap gap-2 justify-center items-center">
       <Tooltip title="Elimina l'article de la teva llista de favorits">
         <IconButton size="small">
           <button
-            className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-red-500 hover:text-white transition-all duration-200"
+            className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-red-500 hover:text-white transition-all duration-200 z-20"
             onClick={() => handleDeleteFavorite(article.articleid)}
           >
             <AiFillDelete size={22} />
