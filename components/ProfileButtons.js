303ff06270f1d4475edc2a5f48d2d2baf747468c
joaquinns <joaquindevv96@gmail.com>
2022-07-16 12:56:49 -0400

    fix warning

--- components/ProfileButtons.js
+++ components/ProfileButtons.js
@@ -77,58 +77,49 @@ export const ProfileButtons = ({ publicationStatus, saleStatus, article }) => {
 
       <div className="flex gap-1 md:gap-4 justify-center items-center">
         <Tooltip title="Marca com article venut">
-          <IconButton size="small">
-            <button
-              className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-greenescola hover:text-white transition-all duration-200"
-              onClick={() => {
-                //console.log("ProfileButtons/sold/article.articleid: ", article.articleid);
-                handleUpdate(article.articleid, 3);
-              }}
-            >
-              <FaRegHandshake size={22} />
-            </button>
+          <IconButton
+            size="small"
+            className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-greenescola hover:text-white transition-all duration-200"
+            onClick={() => {
+              //console.log("ProfileButtons/sold/article.articleid: ", article.articleid);
+              handleUpdate(article.articleid, 3);
+            }}
+          >
+            <FaRegHandshake size={22} />
           </IconButton>
         </Tooltip>
 
         <Tooltip title="Marca com article reservat">
-          <IconButton size="small">
-            <button
-              className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-orangeAMPA hover:text-white transition-all duration-200"
-              onClick={() => {
-                //console.log("ArticleView/reserved/article.articleid: ", article.articleid);
-                handleUpdate(article.articleid, 2);
-              }}
-            >
-              <BsBookmark size={22} />
-            </button>
+          <IconButton
+            size="small"
+            onClick={() => handleUpdate(article.articleid, 2)}
+            className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-orangeAMPA hover:text-white transition-all duration-200"
+          >
+            <BsBookmark size={22} />
           </IconButton>
         </Tooltip>
       </div>
 
       <Tooltip title="Edita el teu article">
-        <IconButton size="small">
-          <button
-            className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-gray-400 hover:text-white transition-all duration-200"
-            onClick={() => {
-              //console.log("ArticleView/article.articleid: ", article.articleid)
-              router.push("/articles/edit/" + article.articleid);
-            }}
-          >
-            <FiEdit3 size={22} />
-          </button>
+        <IconButton
+          size="small"
+          className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-gray-400 hover:text-white transition-all duration-200"
+          onClick={() => {
+            //console.log("ArticleView/article.articleid: ", article.articleid)
+            router.push("/articles/edit/" + article.articleid);
+          }}
+        >
+          <FiEdit3 size={22} />
         </IconButton>
       </Tooltip>
 
       <Tooltip title="Elimina definitivament el teu article">
-        <IconButton size="small">
-          <button
-            className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-red-500 hover:text-white transition-all duration-200"
-            onClick={() =>
-              handleDelete(article.articleid, article.imagerefpath)
-            }
-          >
-            <AiFillDelete size={22} />
-          </button>
+        <IconButton
+          size="small"
+          className="px-2 py-2 rounded font-lato font-bold text-gray-700 hover:bg-red-500 hover:text-white transition-all duration-200"
+          onClick={() => handleDelete(article.articleid, article.imagerefpath)}
+        >
+          <AiFillDelete size={22} />
         </IconButton>
       </Tooltip>
     </div>
